7e5c9c99f0e17ee65e0e7ae66ffbcd4dbf51b679
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-14 14:28:03 +0100

    done

--- configure-passport.js
+++ configure-passport.js
@@ -1,4 +1,58 @@
 'use strict';
 
-// Passport Strategy configuration
+const passport = require('passport');
+const passportLocal = require('passport-local');
 
+const bcrypt = require('bcryptjs');
+// Passport Strategy configuration
+const PassportLocalStrategy = passportLocal.Strategy;
+
+const User = require('./models/user');
+//define a serialization and deserialization process
+
+passport.serializeUser((user, callback) => {
+  callback(null, user._id);
+});
+
+passport.deserializeUser((id, callback) => {
+  User.findById(id)
+    .then(user => callback(null, user))
+    .catch(error => callback(error));
+});
+
+//user sign Up
+passport.use(
+  'sign-up',
+  new PassportLocalStrategy({}, (username, password, callback) => {
+    bcrypt
+      .hash(password, 5)
+      .then(hashAndSalt => {
+        return User.create({
+          username,
+          passwordHash: hashAndSalt
+        });
+      })
+      .then(user => callback(null, user))
+      .catch(error => callback(error));
+  })
+);
+
+//user sign-in
+passport.use('sign-in', 
+  new PassportLocalStrategy({}, (username, password, callback) => {
+    let user;
+    User.findOne({username})
+    .then(document => {
+      user = document;
+      return bcrypt.compare(password, user.passwordHash);
+    })
+    .then(result => {
+      if (result) {
+        callback(null, user);
+      } else {
+        return Promisse.reject(new Error('Password does not match'));
+      }
+    })
+    .catch(error => callback(error));
+  })
+);
